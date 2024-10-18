008cb68e9f18aabcab560ff98555fb53fb112722
James Gambrill <jameswgambrill@icloud.com>
2024-10-18 10:45:25 +0100

    finished

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ let bulb1 = document.querySelector("#lightbulb1") //just think of this as the HT
 let bulb2 = document.querySelector("#lightbulb2")
 let bulb3 = document.querySelector("#lightbulb3")
 
+let count = 0
+
 let subtitle = document.querySelector(".subtitle") // same with this
 
 bulb1.addEventListener("click",function(){ //listens for a click to occur on bulb 1 and runs function (identified by the {})
     
     count++ // count increments by 1 on each click
     subtitle.innerHTML = `You've clicked the lights ${count} times` // the $ works by injecting code into a string of text. The inner html means the html between the <h3></h3> tags.
-    this.classlist.toggle("active") //somehow toggles the css class active on and off to make the background change when clicked.
+    this.classList.toggle("active") //somehow toggles the css class active on and off to make the background change when clicked.
     
 })
 
@@ -17,14 +19,14 @@ bulb2.addEventListener("click",function(){ //listens for a click to occur on bul
     
     count++ // count increments by 1 on each click
     subtitle.innerHTML = `You've clicked the lights ${count} times` // the $ works by injecting code into a string of text. The inner html means the html between the <h3></h3> tags.
-    this.classlist.toggle("active") //somehow toggles the css class active on and off to make the background change when clicked.
+    this.classList.toggle("active") //somehow toggles the css class active on and off to make the background change when clicked.
     
 })
 
-bulb2.addEventListener("click",function(){ //listens for a click to occur on bulb 1 and runs function (identified by the {})
+bulb3.addEventListener("click",function(){ //listens for a click to occur on bulb 1 and runs function (identified by the {})
     
     count++ // count increments by 1 on each click
     subtitle.innerHTML = `You've clicked the lights ${count} times` // the $ works by injecting code into a string of text. The inner html means the html between the <h3></h3> tags.
-    this.classlist.toggle("active") //somehow toggles the css class active on and off to make the background change when clicked.
+    this.classList.toggle("active") //somehow toggles the css class active on and off to make the background change when clicked.
     
 })
